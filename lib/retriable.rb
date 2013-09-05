72c92bb8cdf7253c640fbbe5a31eec4ab54e0273
Markus Krogemann <markus@krogemann.de>
2013-09-05 13:57:06 +0200

    refactor and fix retriable to also work in non-kernel mode

--- lib/retriable.rb
+++ lib/retriable.rb
@@ -1,2 +1,16 @@
 require 'retriable/retry'
-require 'retriable/dsl'
+
+module Retriable
+  extend self
+  def retriable(opts = {}, &block)
+    raise LocalJumpError unless block_given?
+
+    Retry.new do |r|
+      r.tries    = opts[:tries] if opts[:tries]
+      r.on       = opts[:on] if opts[:on]
+      r.interval = opts[:interval] if opts[:interval]
+      r.timeout  = opts[:timeout] if opts[:timeout]
+      r.on_retry = opts[:on_retry] if opts[:on_retry]
+    end.perform(&block)
+  end
+end
