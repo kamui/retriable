8dd52e3920dd53e9753c1ddfb4f2d955c8aecd82
Jack Chu <jack@jackchu.com>
2015-01-30 00:02:47 -0500

    drop named parameters for ruby 1.9.3 support

--- lib/retriable.rb
+++ lib/retriable.rb
@@ -16,18 +16,17 @@ module Retriable
     @config ||= Config.new
   end
 
-  def retriable(
-      tries:             config.tries,
-      base_interval:     config.base_interval,
-      max_interval:      config.max_interval,
-      rand_factor:       config.rand_factor,
-      multiplier:        config.multiplier,
-      max_elapsed_time:  config.max_elapsed_time,
-      intervals:         config.intervals,
-      timeout:           config.timeout,
-      on:                config.on,
-      on_retry:          config.on_retry
-    )
+  def retriable(opts = {})
+    tries             = opts[:tries]            || config.tries
+    base_interval     = opts[:base_interval]    || config.base_interval
+    max_interval      = opts[:max_interval]     || config.max_interval
+    rand_factor       = opts[:rand_factor]      || config.rand_factor
+    multiplier        = opts[:multiplier]       || config.multiplier
+    max_elapsed_time  = opts[:max_elapsed_time] || config.max_elapsed_time
+    intervals         = opts[:intervals]        || config.intervals
+    timeout           = opts[:timeout]          || config.timeout
+    on                = opts[:on]               || config.on
+    on_retry          = opts[:on_retry]         || config.on_retry
 
     start_time = Time.now
     elapsed_time = -> { Time.now - start_time }
--- lib/retriable/exponential_backoff.rb
+++ lib/retriable/exponential_backoff.rb
@@ -6,19 +6,12 @@ module Retriable
     attr_accessor :max_interval
     attr_accessor :rand_factor
 
-    def initialize(
-        tries:            Retriable.config.tries,
-        base_interval:    Retriable.config.base_interval,
-        multiplier:       Retriable.config.multiplier,
-        max_interval:     Retriable.config.max_interval,
-        rand_factor:      Retriable.config.rand_factor
-      )
-
-      @tries          = tries
-      @base_interval  = base_interval
-      @multiplier     = multiplier
-      @max_interval   = max_interval
-      @rand_factor    = rand_factor
+    def initialize(opts = {})
+      @tries         = opts[:tries]         || Retriable.config.tries
+      @base_interval = opts[:base_interval] || Retriable.config.base_interval
+      @max_interval  = opts[:max_interval]  || Retriable.config.max_interval
+      @rand_factor   = opts[:rand_factor]   || Retriable.config.rand_factor
+      @multiplier    = opts[:multiplier]    || Retriable.config.multiplier
     end
 
     def intervals
