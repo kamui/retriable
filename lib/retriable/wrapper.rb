3237aef0f189ad25b76a998970312235ea9a1499
Julik Tarkhanov <me@julik.nl>
2015-07-26 16:15:01 +0200

    Add :methods option and __getobj__ to Retriable::Wrapper

--- lib/retriable/wrapper.rb
+++ lib/retriable/wrapper.rb
@@ -1,25 +1,44 @@
 class Retriable::Wrapper
-  def initialize(with_object, retriable_options)
+  
+  # Creates a new Wrapper that will wrap the messages to the wrapped object with
+  # a +retriable+ block.
+  #
+  # If the :methods option is passed, only the methods in the given array will be
+  # subjected to retries.
+  def initialize(with_object, options_for_retriable = {})
     @o = with_object
-    @retriable_options = retriable_options
+    @methods = options_for_retriable.delete(:methods)
+    @retriable_options = options_for_retriable
   end
   
+  # Returns the wrapped object
+  def __getobj__
+    @o
+  end
+  
+  # Assists in supporting method_missing
   def respond_to_missing?(*a)
     @o.respond_to?(*a)
   end
   
+  # Forwards all methods not defined on the Wrapper to the wrapped object.
   def method_missing(*a)
+    method_name = a[0]
     if block_given?
-      __retrying { @o.public_send(*a){|*ba| yield(*ba)} }
+      __retrying(method_name) { @o.public_send(*a){|*ba| yield(*ba)} }
     else
-      __retrying { @o.public_send(*a) }
+      __retrying(method_name) { @o.public_send(*a) }
     end
   end
   
   private
   
   # Executes a block within Retriable setup with @retriable_options
-  def __retrying
-    Retriable.retriable(@retriable_options) { yield }
+  def __retrying(method_name_on_delegate)
+    if @methods.nil? || @methods.include?(method_name_on_delegate)
+      Retriable.retriable(@retriable_options) { yield }
+    else
+      yield
+    end
   end
 end
--- spec/wrapper_spec.rb
+++ spec/wrapper_spec.rb
@@ -5,6 +5,38 @@ describe Retriable::Wrapper do
     Retriable::Wrapper
   end
   
+  it 'returns the wrapped object via __setobj__' do
+    item = :foo
+    wrapped = subject.new(item)
+    wrapped.must_be_kind_of subject
+    unwrapped = wrapped.__getobj__
+    unwrapped.must_be_kind_of Symbol
+  end
+  
+  it 'supports setting retrying methods explicitly' do
+    raising_class = Class.new do
+      def self.perform_failing_operation
+        @tries_failing ||= 0
+        @tries_failing += 1
+        raise StandardError.new if @tries_failing < 3
+        "success"
+      end
+      
+      def self.perform_non_retrying_operation
+        @tries_non_retrying ||= 0
+        @tries_non_retrying += 1
+        raise StandardError.new if @tries_non_retrying < 2 
+      end
+    end
+    
+    wrapped_raising_class = subject.new(raising_class, methods: [:perform_failing_operation])
+    wrapped_raising_class.perform_failing_operation.must_equal "success"
+    
+    expect do
+      wrapped_raising_class.perform_non_retrying_operation
+    end.must_raise StandardError
+  end
+  
   it "makes 3 tries when retrying block of code raising StandardError with no arguments" do
     $tries = 0
     
