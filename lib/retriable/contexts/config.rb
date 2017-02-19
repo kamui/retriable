a407ca046cb441d4e0e5ed0906430f23a29c0ea7
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2017-02-19 04:42:54 -0800

    Even stricter validation of contexts

--- lib/retriable/contexts/config.rb
+++ lib/retriable/contexts/config.rb
@@ -1,16 +1,23 @@
 module Retriable
   class Config
     def contexts
-      @contexts ||= {}
+      @contexts ||= Contexts.new
     end
 
     def contexts=(envs)
       if envs.is_a?(Hash) && envs.values.all? { |e| e.is_a?(Hash) }
-        envs.each { |_, options| c = self.class.new(options).validate! }
-        @contexts = envs
+        envs.each { |env, options| contexts[env] = options }
       else
         raise ArgumentError, 'contexts must be a hash of hashes'
       end
     end
+
+    class Contexts < Hash
+      def []=(key, val)
+        Retriable::Config.new(val).validate!
+        super(key, val)
+      end
+    end
+    private_constant :Contexts
   end
 end
