33023d6bb4c1e5c8544ea38973f84303e2da9ea0
Olle Jonsson <olle.jonsson@gmail.com>
2017-12-08 16:21:31 +0100

    Travis: Try again

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -2,8 +2,7 @@
 
 HAS_JRUBY=$(ruby -v | grep -o jruby)
 if [[ $HAS_JRUBY = "jruby"  ]]; then
-  echo "Using ${BUNDLE_PATH:-vendor/bundle} for an include path"
-  JRUBY_OPTS="--debug" jruby -I "${BUNDLE_PATH:-vendor/bundle}" spec/test_all.rb
+  JRUBY_OPTS="--debug" BUNDLE_PATH=./vendor/bundle bundle exec jruby spec/test_all.rb
 else
   bundle exec ruby spec/test_all.rb
 fi
