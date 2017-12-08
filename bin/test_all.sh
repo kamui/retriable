5cd0c40c348e545255fed22c1585bf8a7899ac9a
Olle Jonsson <olle.jonsson@gmail.com>
2017-12-08 15:37:01 +0100

    Travis: desperate try

--- bin/test_all.sh
+++ bin/test_all.sh
@@ -2,7 +2,7 @@
 
 HAS_JRUBY=$(ruby -v | grep -o jruby)
 if [[ $HAS_JRUBY = "jruby"  ]]; then
-  ruby -I vendor/bundle spec/test_all.rb
+  JRUBY_OPTS="--debug" jruby -I vendor/bundle spec/test_all.rb
 else
   bundle exec ruby spec/test_all.rb
 fi
--- spec/test_all.rb
+++ spec/test_all.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "spec_helper"
 require_relative "config_spec"
 require_relative "exponential_backoff_spec"
 require_relative "retriable_spec"
