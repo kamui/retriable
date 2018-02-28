879b3221ba4e5e2a8763c8a3d0ee64a279291f6b
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2018-02-28 01:07:34 -0800

    hound

--- spec/exponential_backoff_spec.rb
+++ spec/exponential_backoff_spec.rb
@@ -66,7 +66,7 @@ describe Retriable::ExponentialBackoff do
   end
 
   it "generates 10 non-randomized intervals" do
-    non_random_intervals = 9.times.inject([0.5]) { |memo, i| memo + [memo.last * 1.5] }
+    non_random_intervals = 9.times.inject([0.5]) { |memo, _i| memo + [memo.last * 1.5] }
     expect(described_class.new(tries: 10, rand_factor: 0.0).intervals).to eq(non_random_intervals)
   end
 end
